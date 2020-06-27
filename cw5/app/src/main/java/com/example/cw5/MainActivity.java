90b746df30e98037b5ef201169fa7083bb84bf97
majed531 <66701995+majed531@users.noreply.github.com>
2020-06-27 19:12:21 +0300

    not completed

---
+++ cw5/app/src/main/java/com/example/cw5/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText username = findViewById(R.id.);
+    }
+}
